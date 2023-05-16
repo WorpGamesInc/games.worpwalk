33f6b39da997affbf0fac05d6d529834ab69e5eb
Wiley Worp <theworpofmanytrades@gmail.com>
2023-05-15 20:42:09 -0600

    2048

---
+++ 2048/2048.js
@@ -0,0 +1,44 @@
+
+var board;
+var score = 0;
+var rows = 4;
+var columns = 4;
+
+window.onload = function () {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columbs; c++) {
+            //<div id="0-0"></div>
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c];
+            updateTile(tile, num);
+            document.getElementById("board").append(tile);
+            
+        }
+    }
+}
+
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; //clears the classlist, so tiles can't have multiple classes
+    tile.classList.add("tile");
+    if (num > 0) {
+        tile.innerText = num;
+        if (num <= 4096) {
+            tile.classList.add("x" + num.toString());
+        } else {
+            tile.classList.add("x8192");
+        }
+    }
+}
