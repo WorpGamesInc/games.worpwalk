52da05c5e4aa7e02150addde54aae09013e3839d
Wiley Worp <theworpofmanytrades@gmail.com>
2023-10-29 23:25:41 -0600

    Added...arms?

--- gainzsim/app.js
+++ gainzsim/app.js
@@ -1,6 +1,7 @@
 let gainNumber = 0;
-
+let arms = 1;
 const gainCount = document.getElementById("gainCount");
+
 
 function clickGains() {
     document.querySelectorAll('button')[0].style.background = "red";
@@ -8,18 +9,23 @@ function clickGains() {
         document.querySelectorAll('button')[0].style.background = "blue";
       }, 250);
 
-      gainNumber++;
+      gainNumber+=arms;
       gainCount.textContent = gainNumber;
 
       if (gainNumber > 14.1) {
         document.getElementById("confidence").style.display = "block";
       }
+
+      if (gainNumber > 99.1) {
+        document.getElementById("bothArms").style.display = "block";
+      }
 };
 
-function clickResetGame() {
+
+function clickReset() {
   location.reload();
-  
 }
+
 
 function clickConfidence() {
 
@@ -36,3 +42,17 @@ function clickConfidence() {
       }, 1000);
     }
   }
+
+
+  function clickBothArms() {
+
+    if (gainCount.textContent = gainNumber < 99.1) {
+      gainCount.textContent = gainNumber;
+    }
+  
+    else if (gainCount.textContent = gainNumber > 99.1) {
+      gainCount.textContent = gainNumber = gainNumber - 100;
+  
+       arms = arms+2
+      }
+    }
