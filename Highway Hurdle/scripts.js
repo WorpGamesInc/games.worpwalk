a8c41ec062dcd75d11395cb4a7139a1322210db8
Wiley Worp <theworpofmanytrades@gmail.com>
2023-11-03 19:04:52 -0600

    Update scripts.js

--- Highway Hurdle/scripts.js
+++ Highway Hurdle/scripts.js
@@ -1,7 +1,22 @@
 // Drawing the road
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.beginPath(790, 200); ctx.moveTo(750, 1);
-// ctx.lineTo(750, 499);
-// ctx.lineTo(90, 1);
+ctx.beginPath(); ctx.moveTo(730, 0);
+ctx.lineTo(70, 0);
+ctx.lineTo(30, 500);
+ctx.lineTo(770, 500);
+ctx.lineTo(730, 0);
 ctx.stroke();
+ctx.fill();
+
+// Player
+var player = {
+    x: 200,
+    y: 200,
+    draw: function() {
+        ctx.fillStyle = "green";
+        ctx.fillRect(player.x, player.y - 20, 20, 20);
+    }
+}
+
+player.draw();
