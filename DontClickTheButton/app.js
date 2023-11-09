d6630ab563037da38772f01dd2a97a8dfbc157b0
Wiley Worp <theworpofmanytrades@gmail.com>
2023-11-08 19:34:49 -0700

    Finished Don't Click The Button

--- DontClickTheButton/app.js
+++ DontClickTheButton/app.js
@@ -1,3 +1,23 @@
 function pressButton() {
-    document.getElementById('buttonImg').
+    document.getElementById('buttonImg').src = "/DontClickTheButton/img/buttonPressed.png"
+    var timer = setTimeout(function() {
+        document.getElementById('buttonImg').src = "/DontClickTheButton/img/buttonUnpressed.png";
+      }, 500);
+      var timer = setTimeout(function() {
+        document.getElementById('buttonShown').style.display = 'none';
+        document.getElementById('buttonHidden').style.display = 'block';
+      }, 1500);
+      var timer = setTimeout(function() {
+        document.getElementById('goAhead').style.display = 'block';
+      }, 2500);
+      var timer = setTimeout(function() {
+        document.getElementById('backButton').style.display = 'block';
+      }, 3500);
+}
+
+function goBack() {
+    document.getElementById('buttonHidden').style.display = 'none';
+    document.getElementById('backButton').style.display = 'none';
+    document.getElementById('goAhead').style.display = 'none';
+    document.getElementById('buttonShown').style.display = 'block';
 }
--- scripts.js
+++ scripts.js
@@ -5,3 +5,7 @@ function clickGainzSimulator() {
 function clickNoPast11() {
     window.location.href = '/nopast11';
 }
+
+function clickDCTB() {
+    window.location.href = '/DontClickTheButton';
+}
